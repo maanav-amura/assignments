601e5921c4ff67dc2a8ce4606495c28489221465
maanav-amura <maanav@amuratech.com>
2017-08-08 19:01:54 +0530

    Max Min

--- max_min.rb
+++ max_min.rb
@@ -1,13 +1,16 @@
 #  return the highest and lowest number from a string of space separated numbers
-def high_and_low(array)
-  "#{array.split(' ').min.to_i} #{array.split(' ').max.to_i}"
+def high_and_low(array = nil)
+  array = array.split(' ').map!(&:to_i)
+  "#{array.max} #{array.min}"
 end
 
 puts high_and_low('1 2 3 4 5')
 puts high_and_low('1 2 -3 4 5')
-puts high_and_low("1 2 3 011 10")
+puts high_and_low('1 2 3 011 10')
+puts high_and_low('1 -100 3000 1000 10')
 
-#output
-# 1 5
-# -3 5
-# -5 9
+# output
+# 5 1
+# 5 -3
+# 11 1
+# 3000 -100
