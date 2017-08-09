e37ac66cec611cdc57a8ba77777217a674d340bf
maanav-amura <maanav@amuratech.com>
2017-08-09 13:49:31 +0530

    Sum of consecutive numbers

--- sum_of_n.rb
+++ sum_of_n.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 # returns an Array of length abs(n) + 1
 # produced by taking sum of consecutive integer numbers from 0 to n inclusive
 def sum_of_n(n = nil)
@@ -6,33 +8,13 @@ def sum_of_n(n = nil)
   Array.new(n.abs + 1) { |index| n.negative? ? sum -= index : sum += index }
 end
 
-puts 'Sum of n numbers :: '
-
-n = 5
-p sum_of_n(n)
-n = -5
-p sum_of_n(n)
-n = 11
-p sum_of_n(n)
-n = -7
-p sum_of_n(n)
-n = nil
-p sum_of_n(n)
-n = 0
-p sum_of_n(n)
-n = -1
-p sum_of_n(n)
-n = ''
-p sum_of_n(n)
-
-# Output ::
-#
-# Sum of n numbers ::
-# [0, 1, 3, 6, 10, 15]
-# [0, -1, -3, -6, -10, -15]
-# [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66]
-# [0, -1, -3, -6, -10, -15, -21, -28]
-# nil
-# [0]
-# [0, -1]
-# nil
+p sum_of_n(5)   # [0, 1, 3, 6, 10, 15]
+p sum_of_n(-5)  # [0, -1, -3, -6, -10, -15]
+p sum_of_n(11)  # [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66]
+p sum_of_n(-7)  # [0, -1, -3, -6, -10, -15, -21, -28]
+p sum_of_n(nil) # nil
+p sum_of_n(0)   # [0]
+p sum_of_n(-1)  # [0, -1]
+p sum_of_n('')  # nil
+p Benchmark.measure{sum_of_n(5)}
+# <Benchmark::Tms:0x00000001ba0850 @label="", @real=6.889999895065557e-06, @cstime=0.0, @cutime=0.0, @stime=0.0, @utime=0.0, @total=0.0>
