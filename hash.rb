7f8883a440c3839e6a1a25725bf3e2e6bddaf167
maanav-amura <maanav@amuratech.com>
2017-09-23 14:00:34 +0530

    updating assignments

--- hash.rb
+++
@@ -1,8 +0,0 @@
-# solution({a: 1, b: '2'}) # should return "a = 1,b = 2"
-def print(hash)
-  puts 'Hash :: '
-  puts hash.map { |k, v| "#{k} = #{v}" }.join(', ')
-end
-
-hash = { :font_size => 10, :font_family => "Arial" }
-print hash
